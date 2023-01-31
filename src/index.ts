de8a6d9a569ca3b3e9ef0d91e26ca64b0e099c87
Jean-Baptiste-DP <jbdurand@gmail.com>
2023-01-31 16:49:02 +0100

    response to ws

--- src/index.ts
+++ src/index.ts
@@ -41,25 +41,44 @@ AppDataSource.initialize().then(async () => {
 
             if(data.req && data.req=="connexion" && data.type){
                 if(data.type=="screen"){
+                    console.log("New screen")
                     WsConnection.newScreen(ws)
                     screenService.create({height:100,width:100, ip:"127.0.0.1"})
                 }else{
+                    console.log("New phone")
                     const index = WsConnection.newPhoneClient(ws)
                     response = cursorService.create({idScreen:1, ip:"127.0.0.1", ...index})
                 }
             }else if(data.req && data.req=="move" && data.x!=undefined && data.y!=undefined){
+                console.log("Move")
                 const index = WsConnection.searchIndexFromClient(ws)
                 response = cursorService.move({idScreen:1, idCursor: index},data)
             }else if(data.req && data.req=="chgColor" && data.color!=undefined){
+                console.log("ChgColor")
                 const index = WsConnection.searchIndexFromClient(ws)
                 response = pixelService.create({idScreen:1, idCursor: index}, data)
             }else{
                 console.log("Error in websocket : ")
                 console.log(data)
             }
+
+            if(response.req && response.req=="move" && response.id!=-1){
+                ws.send(JSON.stringify(response))
+                if(WsConnection.screen){
+                    WsConnection.screen.send(JSON.stringify(response))
+                }
+            }
+            else if(response.req && response.req=="chgColor" && response.color!=-1){
+                if(WsConnection.screen){
+                    WsConnection.screen.send(JSON.stringify(response))
+                }
+                for(let i=0; i<WsConnection.nbPhone; i++){
+                    WsConnection.phones[i].send(JSON.stringify(response))
+                }
+            }
         });
     });
 
     server.listen(process.env.PORT, () => console.log(`Lisening on port :`+process.env.PORT))
-    
+
 }).catch(error => console.log(error))
